33a222b558bc1f2172acd86940e1faf6d9057a08
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-09-10 11:59:43 +0530

    refactor: remove unused context and lazy-load component for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import { useSideBar } from './Contexts/SideBarContext'
 import NavBar from './Components/NavBar/NavBar'
 import { Outlet, Route, Routes } from 'react-router-dom'
-import Sell from './Components/NavBar/NavComponents/Sell/Sell'
-import Home from './Pages/Home/Home'
-import SideBar from './Components/Sidebar/SideBar'
-import Books from './Components/NavBar/NavComponents/Books/Books'
-import NotFound from './Pages/NotFound/NotFound'
 import SearchBar from './Components/SearchBar/SearchBar'
-import Card from './Components/ProductCard/Card'
-import { SelectContextProvider } from './Contexts/SelectContext'
-import Products from './Pages/Products/Products'
-import Cart from './Pages/CartPage/Cart'
+
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import('./Pages/Home/Home'))
+const Sell = lazy(() => import('./Components/NavBar/NavComponents/Sell/Sell'))
+const SideBar = lazy(() => import('./Components/Sidebar/SideBar'))
+const Books = lazy(() =>
+  import('./Components/NavBar/NavComponents/Books/Books')
+)
+const NotFound = lazy(() => import('./Pages/NotFound/NotFound'))
+const Card = lazy(() => import('./Components/ProductCard/Card'))
+const Cart = lazy(() => import('./Pages/CartPage/Cart'))
+const Products = lazy(() => import('./Pages/Products/Products'))
 
 const Layout = () => {
   const { sideBar } = useSideBar()
@@ -20,30 +24,32 @@ const Layout = () => {
       <SearchBar />
       <NavBar />
       {sideBar && <SideBar />}
-      <Outlet />
+      <Suspense fallback={<h2>Loading....</h2>}>
+        <Outlet />
+      </Suspense>
     </>
   )
 }
 
 function App() {
   return (
-    <SelectContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="Home" element={<Home />} />
-          <Route path="sell" element={<Sell />} />
-          <Route path="books" element={<Books />} />
-          <Route path="cart" element={<Cart />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
 
-          <Route path="products" element={<Products />}>
-            <Route index element={<Card />} />
-          </Route>
+        <Route path="Home" element={<Home />} />
 
-          <Route path="*" element={<NotFound />} />
+        <Route path="sell" element={<Sell />} />
+        <Route path="books" element={<Books />} />
+        <Route path="cart" element={<Cart />} />
+
+        <Route path="products" element={<Products />}>
+          <Route index element={<Card />} />
         </Route>
-      </Routes>
-    </SelectContextProvider>
+
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
