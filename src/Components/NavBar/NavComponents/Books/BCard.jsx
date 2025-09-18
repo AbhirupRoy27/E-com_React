fc5e0f191672956518a6034a274c70d2306988eb
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-09-18 10:32:16 +0530

    refactor(book): improve card layout and responsiveness with images - Added displaying images - Updated card layout to ensure responsiveness across devices

--- src/Components/NavBar/NavComponents/Books/BCard.jsx
+++ src/Components/NavBar/NavComponents/Books/BCard.jsx
@@ -1,40 +1,41 @@
 import React, { useState } from 'react'
 
 function BCard({ books }) {
-  const [isDesc, setIsDesc] = useState(true)
+  const [isDesc, setIsDesc] = useState(false)
 
-  // <div onClick={() => setIsDesc(!isDesc)}>
-  //       {isDesc ? (
-
-  //       ) : (
-  //
-  //       )}
-  //     </div>
-  // console.log(books)
   return (
     <>
-      <div className="flex flex-col w-[49%] sm:w-[19%] bg-blue-50 p-2 h-max gap-[0.4rem]">
+      <div className="flex flex-col w-[49%] sm:w-[19%] bg-gray-100 p-2 h-max gap-[0.4rem]">
         {isDesc ? (
-          <div className="min-h-[493.9px] sm:min-h-[493.9px]">
-            <p className="mt-[10px]">{books.description}</p>
+          <div className="min-h-[493.9px] sm:min-h-[493.9px] bg-gray-200 p-3 rounded-md">
+            <p className="mt-[10px] tracking-[1px]">{books.description}</p>
             <p
               onClick={() => setIsDesc(!isDesc)}
-              className="mt-[1rem] font-[600] text-red-900"
+              className="mt-[1rem] font-[600] text-red-900 tracking-[1px] ml-2 text-xl"
             >
               Close
             </p>
           </div>
         ) : (
           <>
-            <h2 className="text-xl mt-[2px] font-semibold h-[56px]  ">
+            <h2 className="text-sm sm:text-xl mt-[2px] font-semibold h-[56px] tracking-[1px]">
               {books.title}
             </h2>
-            <div className="w-full min-h-[220px] sm:min-h-[250px] bg-orange-100 rounded-md">
-              <img src="hbvjhv" alt={books.title} className="p-2 rounded-md" />
+            <div className="w-full h-[220px] sm:h-[380px] bg-orange-100 rounded-md">
+              <img
+                src={books.website}
+                alt={books.title}
+                className="w-full h-full rounded-md"
+              />
             </div>
 
-            <h3 className="font-semibold min-h-[48px]">{books.author}</h3>
-            <p className="font-[500]" onClick={() => setIsDesc(!isDesc)}>
+            <h3 className="font-semibold  min-h-[72px] sm:min-h-[48px] tracking-[1px]">
+              {books.author}
+            </h3>
+            <p
+              className="font-[500] tracking-[1px]"
+              onClick={() => setIsDesc(!isDesc)}
+            >
               Read Discription...
             </p>
 
@@ -42,7 +43,10 @@ function BCard({ books }) {
               <p className="text-xl font-semibold">{books.pages}</p>
               <p>Total Pages</p>
             </div>
-            <button className="bg-pink-100 p-3 mt-[6px] rounded-md font-semibold">
+            <button
+              className="bg-yellow-300 p-3 mt-[6px] rounded-md font-semibold tracking-[1px] opacity-60"
+              disabled
+            >
               Read now
             </button>
           </>
