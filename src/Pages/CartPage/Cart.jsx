345210d17cd84c9566391cbc7d61b64970cce0fc
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-09 12:50:50 +0530

    fixed the cart layout and reponsiveness.

--- src/Pages/CartPage/Cart.jsx
+++ src/Pages/CartPage/Cart.jsx
@@ -8,27 +8,28 @@ const BillCard = lazy(() => import('./ComponetntsCart/Bill/BillCard'))
 
 function CartLayout({ cart, total }) {
   return (
-    <>
-      <div className=" flex flex-col md:flex-row justify-center gap-2 items-center sm:items-start px-[1%] lg:px-[2%] 2xl:px-[12%] min-w-[100vw] sm:min-w-[640px] md:min-w-[768px] lg:min-w-[1024px] xl:min-w-[1280px] overflow-hidden">
-        <div className="flex flex-col gap-[0.6rem] mb-2 ">
-          {cart.map((item) => (
-            <div
-              key={item.id || item._id}
-              className="flex justify-center w-full"
-            >
-              <CartCard item={item} />
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col md:flex-row justify-center gap-2 sm:py-4 min-h-screen">
+      <div className="flex flex-col gap-[0.6rem] mb-2 ">
+        {cart.map((item) => (
+          <div
+            key={item.id || item._id}
+            className="flex justify-center items-center w-full"
+          >
+            <CartCard item={item} />
+          </div>
+        ))}
+      </div>
+
+      <div className="">
         {cart.length > 0 && (
           <Suspense>
-            <div className="flex justify-center w-full md:w-max rounded-md bg-gray-100 h-[100%] p-[1%] my-[2%] md:my-0">
+            <div className="flex justify-center w-full md:w-max rounded-md bg-gray-100 h-fit px-4 py-2">
               <BillCard total={total} />
             </div>
           </Suspense>
         )}
       </div>
-    </>
+    </div>
   )
 }
 
