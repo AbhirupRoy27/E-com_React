9c07d937a32464ee812e7974524459e3f2c57ea2
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-13 13:48:04 +0530

    feat: enhance product loading state and improve brand selection UI

--- src/Components/ProductCard/Card.jsx
+++ src/Components/ProductCard/Card.jsx
@@ -93,8 +93,12 @@ function Card() {
       ))}
     </>
   ) : (
-    <h1 className="flex w-[100%] h-[80vh] justify-center items-center text-2xl">
-      Sorry Can't Load
+    <h1 className="flex flex-col gap-3 w-screen h-[80vh] justify-center items-center text-2xl">
+      <img
+        src="https://res.cloudinary.com/dfuhnqbap/image/upload/v1765612425/box_kynrxh.png"
+        alt="No Product"
+      />
+      <b className="text-4xl text-gray-600">Sorry! No Such Product</b>
     </h1>
   )
 }
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -2,11 +2,21 @@ import { Outlet } from 'react-router-dom'
 import { useProducts } from '../../Contexts/ProductContext'
 import Page from '../../Components/PageNavigation/Page'
 
+const Brands = ['All', 'Zudio', 'Puma', 'Nike', 'Addidas', 'Zara', 'Campus']
 
 export const BrandDropList = () => {
   const { setFilteredProducts, products } = useProducts()
 
-  const handleBs = (e) => {
+  const getBrands = (e) => {
+    if (e.target.localName == 'button') {
+      let text =
+        e.target.innerText.charAt(0) + e.target.innerText.slice(1).toLowerCase()
+      // console.log(text)
+      return setFilteredProducts(
+        text === 'All' ? products : products.filter((i) => i.name === text)
+      )
+    }
+
     setFilteredProducts(
       e.target.value === 'All'
         ? products
@@ -14,11 +24,22 @@ export const BrandDropList = () => {
     )
   }
   return (
-    <div className="flex items-center gap-[0.4rem] ">
-      <p className="font-semibold ">Select Brand:</p>
+    <div className="flex items-center gap-[0.4rem] px-3 lg:px-10">
+      <p className="font-semibold font-mono text-xl ">Select Brand:</p>
+      <div className="hidden sm:flex gap-2 overflow-x-scroll no-scrollbar">
+        {Brands.map((b, idx) => (
+          <button
+            key={idx}
+            onClick={getBrands}
+            className="bg-gray-900/20 hover:bg-gray-900/30 min-w-[100px] text-black font-semibold py-2 rounded-2xl border-dashed border-2 border-black/40 uppercase active:scale-95"
+          >
+            {b}
+          </button>
+        ))}
+      </div>
       <select
-        className="p-[0.4rem] min-w-[100px] font-semibold focus:outline-none focus:ring-0 focus:border-transparent bg-[rgba(62,62,62,0.212)] rounded-md"
-        onChange={handleBs}
+        className="sm:hidden p-[0.4rem] min-w-[100px] font-semibold focus:outline-none focus:ring-0 focus:border-transparent bg-[rgba(62,62,62,0.212)] rounded-md"
+        onChange={getBrands}
       >
         <option defaultValue={'All'}>All</option>
         <option>Zudio</option>
@@ -33,11 +54,11 @@ export const BrandDropList = () => {
 function Products() {
   return (
     <div className="w-full flex ">
-      <div className="flex w-full h-full flex-col bg-[rgba(62,62,62,0.212)] min-w-[100vw] sm:min-w-[640px] md:min-w-[768px] lg:min-w-[1024px] xl:min-w-[1280px]">
+      <div className="flex w-full h-full flex-col bg-[rgba(62,62,62,0.212)] min-w-[354px] overflow-hidden">
         <div className="p-2 flex gap-2">
           <BrandDropList />
         </div>
-        <div className="flex flex-wrap gap-1 md:gap-2 p-1 sm:p-2 sm:px-[3%] min-h-[90vh] mb-4 min-w-[350px]">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:lg:grid-cols-5  gap-1 md:gap-2 px-2 sm:px-10 min-h-[90vh] mb-4">
           <Outlet />
         </div>
         <Page />
