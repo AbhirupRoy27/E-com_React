84d48f22c6e20751b1a487aba2e186476b92434c
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-18 11:41:12 +0530

    feat: refactor Orders component for improved layout and styling; remove unused state

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Orders() {
-  const [orders, setOrders] = useState('Make Your First Order with NextGen')
   // setOrders('Make Your First Order with NextGen')
   const navigate = useNavigate()
   return (
-    <div className="min-h-[85vh] flex justify-center items-center flex-col min-w-[100vw] sm:min-w-[640px] md:min-w-[768px] lg:min-w-[1024px] xl:min-w-[1280px]">
-      <p className="text-2xl pb-1">{orders}</p>
+    <div className="min-h-screen flex justify-center items-center flex-col min-w-screen">
+      <img
+        src="https://res.cloudinary.com/dfuhnqbap/image/upload/v1766037817/delivery-order_hnrum6.png"
+        alt="order"
+      />
+      <p className="text-4xl pb-1 font-bold tracking-wider">
+        Make Your First Order with NextGen
+      </p>
       <button
-        className="mt-4 py-1 px-3 hover:border bg-slate-200 rounded"
+        className="mt-4 py-2 px-5 hover:bg-gray-700/60 bg-gray-700 rounded font-bold tracking-wider transition-all duration-300 "
         onClick={() => navigate('/login')}
       >
         Login / Signup
