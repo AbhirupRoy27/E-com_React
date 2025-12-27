a12b6ab829de68608df28e0a483369315081fbfb
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-27 23:32:36 +0530

    started wishlist design

--- src/features/wishlist/Pages/WishlistPage.jsx
+++ src/features/wishlist/Pages/WishlistPage.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function WishlistPage() {
   return (
-    <div className="h-screen min-w-[354px]">
-      <h1>Wishlist</h1>
+    <div className="h-screen min-w-[354px] sm:mt-5 ">
+      <h1 className="capitalize text-4xl pb-3 tracking-wider font-light text-nowrap">
+        My Wishlist
+      </h1>
+      <section>
+        <div className="flex gap-2 ">
+          {['', 'Product Name', 2, 3].map((item) => (
+            <h3 key={item} className={`${item == '' && 'min-w-[100px]'}`}>
+              {item}
+            </h3>
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
