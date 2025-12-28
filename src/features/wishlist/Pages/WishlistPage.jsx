a27bbf25fdc080166712108e1bdb2bc4977ce3d0
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-28 20:21:02 +0530

    changed the wishlist page with new state items.

--- src/features/wishlist/Pages/WishlistPage.jsx
+++ src/features/wishlist/Pages/WishlistPage.jsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react'
 import NoItemWishlist from '../Components/NoItemWishlist'
 import { useWishlist } from '../Context/WishlistContext'
 
@@ -5,7 +6,7 @@ function WishlistPage() {
   const { wishlistItems } = useWishlist()
   console.log(wishlistItems)
   return (
-    <div className="h-screen min-w-[354px] sm:mt-5 overflow-x-scroll">
+    <div className="h-[70vh] min-w-[354px] sm:mt-5 overflow-x-scroll">
       <h1 className="capitalize text-4xl pb-3 tracking-wider font-light text-nowrap">
         My Wishlist
       </h1>
@@ -18,7 +19,7 @@ function WishlistPage() {
             <div className="grid grid-cols-[80px_120px_300px_200px_160px_200px] lg:grid-cols-[80px_160px_500px_250px_200px_250px] border-y-1 border-white/20 w-full text-center py-5 gap-3">
               <button></button>
               <div></div>
-              <h3>Product Name</h3>
+              <h3>Product Details</h3>
               <h3>Unit Price</h3>
               <h3>Stock Status</h3>
               <h3>Actions</h3>
@@ -28,17 +29,30 @@ function WishlistPage() {
                 className="grid grid-cols-[80px_120px_300px_200px_160px_200px] lg:grid-cols-[80px_160px_500px_250px_200px_250px] border-b-1 border-white/20 text-center py-5 gap-3 no-scrollbar"
                 key={item}
               >
-                <button>X</button>
-                <div className="flex justify-center items-center w-full">
-                  <img src="https://m.media-amazon.com/images/G/31/IMG25/Fashion/BAU/Flip/EOSS/HalosNonSelected/Men._SS300_QL85_FMpng_.png" />
+                <button>
+                  <X />
+                </button>
+                <div className="flex justify-center items-center w-full h-[160px] overflow-hidden">
+                  <img src={item.coverImage} />
                 </div>
-                {['Product Name', 'Unit Price', 'Stock Status', 'Actions'].map(
-                  (item) => (
-                    <h3 className="flex justify-center items-center w-full">
-                      {item}
-                    </h3>
-                  )
-                )}
+
+                <h3 className="flex justify-center items-center w-full">
+                  {item.description}
+                </h3>
+                <h3 className="flex justify-center gap-2 items-center w-full">
+                  <b className="text-xl font-light">{item.price}.00</b>
+                  <span className="line-through text-[14px] font-extralight">
+                    {item.originalPrice}.00
+                  </span>
+                </h3>
+                <h3 className="flex justify-center items-center w-full text-xl tracking-wider">
+                  {item.stock == 0 ? 'Out Of Stock' : 'In Stock'}
+                </h3>
+                <div className="flex justify-center items-center w-full">
+                  <button className="bg-white/90 hover:bg-white py-2 px-5 text-gray-950 font-bold">
+                    Add to Cart
+                  </button>
+                </div>
               </div>
             ))}
           </div>
