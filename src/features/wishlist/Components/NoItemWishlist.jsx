c656693d0edc7d11b82e523f138e72937b0eb260
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-28 12:40:13 +0530

    feat: a new placeholer for an empty wishlist.

---
+++ src/features/wishlist/Components/NoItemWishlist.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NoItemWishlist() {
+  return (
+    <div className="flex justify-center items-center min-h-[60vh]">
+      <div>
+        <img src="https://res.cloudinary.com/dfuhnqbap/image/upload/v1765612425/box_kynrxh.png" />
+      </div>
+      <h2 className="text-3xl">Nothing in the Wishlist</h2>
+    </div>
+  )
+}
+
+export default NoItemWishlist
