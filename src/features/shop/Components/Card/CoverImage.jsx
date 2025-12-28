b99999335a63f6d1026b6b36ab71413de610b6d0
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-28 20:20:27 +0530

    feat: attach the handler to the card.

--- src/features/shop/Components/Card/CoverImage.jsx
+++ src/features/shop/Components/Card/CoverImage.jsx
@@ -2,10 +2,13 @@ import { useNavigate } from 'react-router-dom'
 import handleGoToItemPage from '../../../../shared/utils/handleGoToItemPage'
 import { Heart } from 'lucide-react'
 import { useState } from 'react'
+import handleWishlist from '../../utils/handleWishlist'
+import { useWishlist } from '../../../wishlist/Context/WishlistContext'
 
 function CoverImage({ product }) {
   const [isClicked, setIsClicked] = useState(false)
   const navigate = useNavigate()
+  const { wishlistItems } = useWishlist()
   return (
     <div className="w-full">
       <div className="relative w-full h-70 rounded-tr-md rounded-tl-md overflow-hidden">
@@ -18,7 +21,9 @@ function CoverImage({ product }) {
 
         <button
           className="absolute top-3 right-3 bg-white/70 px-2 py-2 rounded-full text-sm hover:bg-white/80 transition active:scale-105"
-          onClick={() => setIsClicked(!isClicked)}
+          onClick={() =>
+            handleWishlist(setIsClicked, isClicked, product, wishlistItems)
+          }
         >
           <Heart
             color={`${isClicked ? 'red' : 'black'}`}
