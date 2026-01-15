e492fd414f92cf6419879e00eb0ffd1a3c722b9d
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-15 20:16:34 +0530

    feat: adjust NavbarPayment styles for improved responsiveness

--- src/features/Payment/Components/NavbarPayment.jsx
+++ src/features/Payment/Components/NavbarPayment.jsx
@@ -3,20 +3,15 @@ import { CircleUser } from 'lucide-react'
 function NavbarPayment() {
   return (
     <div className="bg-gray-900 flex justify-between items-center px-2 sm:px-6 py-3">
-      <p className="text-4xl text-gray-300 font-semibold text-shadow-lg text-shadow-white/30 tracking-wide font-mono">
+      <p className="text-3xl lg:text-4xl text-gray-300 font-semibold text-shadow-lg text-shadow-white/30 tracking-wide font-mono">
         NG
       </p>
-      <p className="text-3xl lg:text-4xl text-white tracking-widest">
+      <p className="text-2xl sm:text-3xl lg:text-4xl text-white tracking-widest">
         Checkout
       </p>
       <div className="w-[53px] h-[53px] bg-slate-100 rounded-full flex justify-center items-center">
         <CircleUser color="black" size={50} />
       </div>
-      {/* <img
-        src="#"
-        alt="Cart"
-        className="w-[53px] h-[53px] bg-slate-100 rounded-full px-1 py-4"
-      /> */}
     </div>
   )
 }
