1bd2c73d4c16e29c9ac3a7aeffa391a7cd03c887
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-15 20:29:47 +0530

    feat: restructure CheckOutLayout to include CheckoutForm and OrderTotal components

--- src/features/Payment/Layout/CheckOutLayout.jsx
+++ src/features/Payment/Layout/CheckOutLayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import NavbarPayment from '../Components/NavbarPayment'
+import CheckoutForm from './CheckoutForm'
+import OrderTotal from '../Components/OrderTotal'
 
 function CheckOutLayout() {
   return (
     <>
       <NavbarPayment />
+      <div className="flex flex-col sm:flex-row gap-4 px-2 sm:px-6 lg:px-20 py-4">
+        <CheckoutForm />
+        <OrderTotal />
+      </div>
     </>
   )
 }
